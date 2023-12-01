5f962da11e14ece886253d1b9f1ea1d65cf83bac
tyrannus00 <dadawdawdadawdwa@gmail.com>
2023-12-01 19:19:48 +0100

    guh

--- src/main/java/de/tyrannus/adventofcode/secrets/SessionCookie.java
+++
@@ -1,7 +0,0 @@
-package main.java.de.tyrannus.adventofcode.secrets;
-
-public class SessionCookie {
-    public static String get() {
-        throw new RuntimeException("You need to return your session cookie here!");
-    }
-}
